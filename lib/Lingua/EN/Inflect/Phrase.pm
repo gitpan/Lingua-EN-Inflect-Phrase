0c96b83c2a42e764c5985cd22ad3920f32b8d945
Rafael Kitover <rkitover@gmail.com>
2011-10-10 22:02:50 +0000

    Import of RKITOVER/Lingua-EN-Inflect-Phrase-0.06 from CPAN.
    gitpan-cpan-distribution: Lingua-EN-Inflect-Phrase
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         RKITOVER/Lingua-EN-Inflect-Phrase-0.06.tar.gz
    gitpan-cpan-author:       RKITOVER
    gitpan-cpan-maturity:     released

--- lib/Lingua/EN/Inflect/Phrase.pm
+++ lib/Lingua/EN/Inflect/Phrase.pm
@@ -12,7 +12,7 @@ Lingua::EN::Inflect::Phrase - Inflect short English Phrases
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
@@ -101,8 +101,8 @@ sub _inflect {
     my @pos = ($-[1], $+[1]);
     my $inflected_noun;
 
-# special case phrases like "2 right braces"
-    if (lc($noun) eq 'right') {
+# special case phrases like "2 right braces" or "2 negative acknowledges"
+    if ($noun =~ /^(?:right|negative)\z/i) {
       (($noun) = $tagged =~ m{$NOUN_OR_VERB (?!.*/(?:NN|CD|JJ|VB[A-Z]?).*/(?:CC|IN)) .* /(?:CC|IN)}x) or
       (($noun) = $tagged =~ m{$NOUN_OR_VERB (?!.*/(?:NN|CD|JJ|VB[A-Z]?))}x);
 
